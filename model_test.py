b5d102a8d4982f8f2bc63a07846d13525d935a5e
cny <chony5093@gmail.com>
2023-11-23 18:18:50 +0900

    add new

---
+++ model_test.py
@@ -0,0 +1,19 @@
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, HfArgumentParser, TrainingArguments, pipeline
+
+
+# Tokenizer
+llama_tokenizer = AutoTokenizer.from_pretrained(base_model_name, trust_remote_code=True)
+llama_tokenizer.pad_token = llama_tokenizer.eos_token
+llama_tokenizer.padding_side = "right" 
+
+#model and tokenizer name
+base_model_name = "NousResearch/Llama-2-7b-chat-hf"
+refined_model = "llama-2-7b-mlabonne-enhanced"
+
+refined_model_path = "/content/llamma-2-7b-mlabonne-enhanced"
+
+# Generate Text
+query = "뭐하냐"
+text_gen = pipeline(task="text-generation", model=refined_model_path, tokenizer=llama_tokenizer, max_length=200)
+output = text_gen(f"<s>[INST] {query} [/INST]")
+print(output[0]['generated_text'])
